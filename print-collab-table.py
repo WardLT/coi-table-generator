51c754029dc60b1a16f39e90bdfb2d23447fa24c
Logan Ward <ward.logan.t@gmail.com>
2019-08-12 12:15:33 -0500

    Added ability to print out BES-format table
    Last name, first name, institution
    
    In CSV format so I can paste it into Word

--- print-collab-table.py
+++ print-collab-table.py
@@ -5,6 +5,8 @@ import argparse
 # Read in the command-line options
 parser = argparse.ArgumentParser()
 parser.add_argument('--date', help='Date of proposal submission in MM-DD-YYYY', type=str, default=None)
+parser.add_argument('--format', help='Format of the output', type=str, default='latex',
+                    choices=['latex', 'bes'])
 args = parser.parse_args()
 
 # Get the date of the proposal
@@ -23,20 +25,30 @@ print('{} total collaborators'.format(len(collabs)))
 collabs = collabs[collabs['Last Collaboration'] > date - timedelta(weeks=48*4)]
 print('{} relevant collaborators'.format(len(collabs)))
 
+# Compute first and last names
+def get_first_and_surname(name):
+    x = name.split()
+    return x[-1], ' '.join(x[:-1])
+collabs['surname'], collabs['first_name'] = zip(*collabs['Name'].apply(get_first_and_surname))
+
 # Sort by surname
-collabs['surname'] = collabs['Name'].apply(lambda x: x.split()[-1])
 collabs.sort_values('surname', ascending=True, inplace=True)
 
 # Get the advisors and advisees
 advis = pd.read_excel('collaborators.xlsx', sheet_name='Mentorship')
 
-# Save to disk
-with open('collabs.tex', 'w') as fp:
-    print('\\subsection*{Co-authors}\n\n', file=fp)
-    collabs[['Name', 'Present Institution']].to_latex(fp, index=False, longtable=True)
-    
-    print('\\subsection*{Co-editors}\n\nNone.\n', file=fp)
-    
-    print('\\subsection*{Advisors and Advisees}\n', file=fp)
-    advis[['Name', 'Present Institution', 'Relationship']].to_latex(fp, index=False, longtable=True)
+# Write in desired formats
+if args.format == 'latex':
+    with open('collabs.tex', 'w') as fp:
+        print('\\subsection*{Co-authors}\n\n', file=fp)
+        collabs[['Name', 'Present Institution']].to_latex(fp, index=False, longtable=True)
+        
+        print('\\subsection*{Co-editors}\n\nNone.\n', file=fp)
+        
+        print('\\subsection*{Advisors and Advisees}\n', file=fp)
+        advis[['Name', 'Present Institution', 'Relationship']].to_latex(fp, index=False, longtable=True)
+elif args.format == 'bes':
+    collabs[['surname', 'first_name', 'Present Institution']].to_csv('bes_table.csv', index=False)
+else:
+    raise NotImplementedError(f'{args.format} not a recognized format')
     
